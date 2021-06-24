4239116be8544abed1f20262bd9e46a8c1e17226
Xhappa <fakedegen@protonmail.com>
2021-06-24 18:37:29 -0400

    Pixel image generator

---
+++ PixelImage.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class PixelImage{
+  public static void main(String args[])throws IOException{
+    BufferedImage img = null;
+    File f = null;
+
+    //read image
+    try{
+      f = new File("INSERTIMAGE");
+      img = ImageIO.read(f);
+    }catch(IOException e){
+      System.out.println(e);
+    }
+
+    /**
+     * Since, Sample.jpg is a single pixel image so, we will
+     * not be using the width and height variable in this
+     * project.
+     */
+
+    //get pixel value
+    int p = img.getRGB(100,100);
+
+    //get alpha
+    int a = (p>>24) & 0xff;
+
+    //get red
+    int r = (p>>16) & 0xff;
+
+    //get green
+    int g = (p>>8) & 0xff;
+
+    //get blue
+    int b = p & 0xff;
+
+    /**
+     * to keep the project simple we will set the ARGB
+     * value to 255, 100, 150 and 200 respectively.
+     */
+    a = 255;
+    r = 100;
+    g = 150;
+    b = 200;
+
+    //set the pixel value
+    p = (a<<24) | (r<<16) | (g<<8) | b;
+    img.setRGB(0, 0, p);
+
+    //write image
+    try{
+      f = new File("Output.jpg");
+      ImageIO.write(img, "jpg", f);
+    }catch(IOException e){
+      System.out.println(e);
+    }
+  }//main() ends here
+}//class ends here
